be0e0bdccb5d90c5cf18060acc14889ee0e2abd4
Flinn Gendall <email": null>
2015-11-10 11:57:44 +1300

    add sockets, give bot command paint to change the index pages background

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-var tayBot = require('./slackBot.js')
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var tayBot = require('./slackBot.js')(io)
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(){
+  console.log('a user connected to tayBot')
+})
+
+http.listen(3000, function(){
+  console.log('server is listening on *:3000')
+})
--- slackbot.js
+++ slackbot.js
@@ -1,43 +1,48 @@
 var dotenv = require('dotenv')
 dotenv.load()
 var Slack = require('slack-client')
-var wit = require('node-wit')
-var fs = require('fs')
 
 slackToken = process.env.SLACK_KEY
 autoRecconect = true
 autoMark = true
 
-slackBot = new Slack(slackToken, autoRecconect, autoMark)
+function slackBot(io){
+  slackBot = new Slack(slackToken, autoRecconect, autoMark)
 
-slackBot.on('open', function(){
-  console.log("Connected to "+slackBot.team.name+" as "+slackBot.self.name)
-})
+  slackBot.on('open', function(){
+    console.log("Connected to "+slackBot.team.name+" as "+slackBot.self.name)
+  })
 
-slackBot.on('message', function(message){
-  channel = slackBot.getChannelGroupOrDMByID(message.channel)
-  user = slackBot.getUserByID(message.user)
-  if(message.text.indexOf("hate")>-1){
-    response = "Haters gonna hate, hate, hate, hate, hate."
-  }
-  else if(message.text.indexOf("play")>-1){
-    response = "Players gonna play, play, play, play play."
-  }
-  else if(message.text.indexOf("fake")>-1){
-    response = "Fakers gonna fake, fake, fake, fake, fake."
-  }
-  else if(message.text.indexOf("heartbreak")>-1){
-    response = "Heartbreakers gonna break, break, break, break, break."
-  }
-  else{response = "Sorry, I don't know what you mean"}
-  channel.send(response)
-})
+  slackBot.on('message', function(message){
+    channel = slackBot.getChannelGroupOrDMByID(message.channel)
+    user = slackBot.getUserByID(message.user)
+    if(message.text.indexOf("hate")>-1){
+      response = "Haters gonna hate, hate, hate, hate, hate."
+    }
+    else if(message.text.indexOf("play")>-1){
+      response = "Players gonna play, play, play, play play."
+    }
+    else if(message.text.indexOf("fake")>-1){
+      response = "Fakers gonna fake, fake, fake, fake, fake."
+    }
+    else if(message.text.indexOf("heartbreak")>-1){
+      response = "Heartbreakers gonna break, break, break, break, break."
+    }
+    else if(message.text.indexOf("paint")>-1){
+      colour = '#ff0000'
+      // console.log(io)
+      io.emit('paint', colour)
+      response = 'Painting that for you'
+    }
+    else{response = "Sorry, I don't know what you mean"}
+    channel.send(response)
+  })
 
-slackBot.on('error', function(err){
-  console.log("Error", err)
-})
+  slackBot.on('error', function(err){
+    console.log("Error", err)
+  })
 
-slackBot.login()
-
+  slackBot.login()
+}
 
 module.exports = slackBot
