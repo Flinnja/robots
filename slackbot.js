73e020f97deba5cb2c92f253eb15cae14c6246e2
Flinn Gendall <email": null>
2015-11-10 14:31:14 +1300

    make taybot only respond to messages that include her name

--- slackbot.js
+++ slackbot.js
@@ -16,26 +16,28 @@ function slackBot(io){
   slackBot.on('message', function(message){
     channel = slackBot.getChannelGroupOrDMByID(message.channel)
     user = slackBot.getUserByID(message.user)
-    if(message.text.indexOf("hate")>-1){
-      response = "Haters gonna hate, hate, hate, hate, hate."
+    if(message.text.indexOf('taybot')>-1){
+      if(message.text.indexOf("hate")>-1){
+        response = "Haters gonna hate, hate, hate, hate, hate."
+      }
+      else if(message.text.indexOf("play")>-1){
+        response = "Players gonna play, play, play, play play."
+      }
+      else if(message.text.indexOf("fake")>-1){
+        response = "Fakers gonna fake, fake, fake, fake, fake."
+      }
+      else if(message.text.indexOf("heartbreak")>-1){
+        response = "Heartbreakers gonna break, break, break, break, break."
+      }
+      else if(message.text.indexOf("paint")>-1){
+        colour = '#ff0000'
+        // console.log(io)
+        io.emit('paint', colour)
+        response = 'Painting that for you'
+      }
+      else{response = "Sorry, I don't know what you mean"}
+      channel.send(response)
     }
-    else if(message.text.indexOf("play")>-1){
-      response = "Players gonna play, play, play, play play."
-    }
-    else if(message.text.indexOf("fake")>-1){
-      response = "Fakers gonna fake, fake, fake, fake, fake."
-    }
-    else if(message.text.indexOf("heartbreak")>-1){
-      response = "Heartbreakers gonna break, break, break, break, break."
-    }
-    else if(message.text.indexOf("paint")>-1){
-      colour = '#ff0000'
-      // console.log(io)
-      io.emit('paint', colour)
-      response = 'Painting that for you'
-    }
-    else{response = "Sorry, I don't know what you mean"}
-    channel.send(response)
   })
 
   slackBot.on('error', function(err){
