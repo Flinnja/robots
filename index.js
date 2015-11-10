993e58df941e6f3ddd68fa8a15ad63c416b1bc3a
Flinn Gendall <email": null>
2015-11-11 10:01:39 +1300

    serve public folder to the static page, implement index.css

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var app = require('express')()
+var express = require('express')
+var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var tayBot = require('./src/slackBot.js')(io)
+
+app.use(express.static('public'));
 
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html')
